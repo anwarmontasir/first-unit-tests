647a86612105831354954824e5051ee4a46c9384
anwarmontasir <anwarmontasir@gmail.com>
2023-01-21 13:35:23 -0800

    9 running tests

--- functions.js
+++ functions.js
@@ -1,9 +1,30 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(x, y) {
+    return x + y;
+}
+
+function sayHello(name) {
+    if (name !== 'Will') {
+        return `Hi there ${name}!`;
+    } else {
+        return `No more testing ${name}!`;
+    }
+}
+
+function buildCar(carColor, carType) {
+    let car = {};
+    if (carColor) {
+        car.color = carColor;
+    }
+    if (carType) {
+        car.type = carType;
+    }
+    console.log('car!!!', car);
+    return car;
 }
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar
 }
